a24975f5996512567143130b76db05c8bfe39a0a
Benz Chanyanuch Somprakit <102774007+csomprakit@users.noreply.github.com>
2024-02-04 22:33:51 -0800

    Added recipe_database.dart

---
+++ lib/database/recipe_database.dart
@@ -0,0 +1,12 @@
+import 'package:floor/floor.dart';
+import 'dart:async';
+import 'package:sqflite/sqflite.dart' as sqflite;
+import 'recipe_dao.dart';
+import 'recipe_entity.dart';
+
+part 'recipe_database.g.dart';
+
+@Database(version: 1, entities: [RecipeEntity])
+abstract class RecipeDatabase extends FloorDatabase {
+  RecipeDao get recipeDao;
+}
