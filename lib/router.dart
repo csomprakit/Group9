de02125de4a5d0c062da6283baf29fba8200ba8b
Benz Chanyanuch Somprakit <102774007+csomprakit@users.noreply.github.com>
2024-02-26 05:37:37 -0800

    Added router.dart

---
+++ lib/router.dart
@@ -0,0 +1,43 @@
+import 'package:go_router/go_router.dart';
+import 'package:recipe_book_app/ReadPage.dart';
+import 'package:recipe_book_app/SettingsPage.dart';
+import 'HomePage.dart';
+import 'database/recipe_database.dart';
+
+class AppRouter
+{
+  late final RecipeDatabase database;
+  AppRouter({required this.database});
+
+  GoRouter getRouter()
+  {
+    return GoRouter(
+      initialLocation: '/home',
+      routes: [
+        GoRoute(
+          path: '/home',
+          builder: (context, routerState)
+          {
+            return HomePage(database: this.database);
+          },
+        ),
+        GoRoute(
+          path: '/search',
+          builder: (context, routerState)
+          {
+            return ReadPage(database: database);
+          },
+        ),
+        GoRoute(
+          path: '/settings',
+          builder: (context, routerState)
+          {
+            return SettingsPage();
+          },
+        )
+      ],
+    );
+  }
+
+}
+
