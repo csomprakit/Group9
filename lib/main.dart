9480c604b3bd948b0d1ea665e2f6bdb99cfa8d7f
Benz Chanyanuch Somprakit <102774007+csomprakit@users.noreply.github.com>
2024-02-28 23:00:16 -0800

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_book_app/database/recipe_database.dart'; // Import your recipe database files
 import 'router.dart';
-
+import 'database/recipe_dao.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   RecipeDatabase database = await $FloorRecipeDatabase.databaseBuilder('recipe.db').build();
-  runApp(MyApp(database: database));
+  final dao = database.recipeDao;
+  runApp(MyApp(dao: dao));
 }
 
 class MyApp extends StatelessWidget {
-  final RecipeDatabase database;
+  final RecipeDao dao;
 
-  const MyApp({required this.database, Key? key}) : super(key: key);
+  const MyApp({ required this.dao, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final appRouter = AppRouter(database: database);
+    final appRouter = AppRouter(dao: this.dao);
     final router = appRouter.getRouter();
     return MaterialApp.router(
       title: 'Recipe App',
