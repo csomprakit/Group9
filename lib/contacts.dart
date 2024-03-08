502409333f5c89ad32e8778ef967b0649c4d7da1
benz <csomprakit@seattleu.edu>
2024-03-08 15:56:41 -0800

    Added contacts.dart

---
+++ lib/contacts.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_share/flutter_share.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:fast_contacts/fast_contacts.dart';
+
+class importContacts extends StatefulWidget {
+  const importContacts({super.key});
+
+  @override
+  State<importContacts> createState() => _importContactsState();
+}
+
+class _importContactsState extends State<importContacts> {
+
+  Future<List<Contact>> getContacts() async {
+    bool isGranted = await Permission.contacts.status.isGranted;
+    if (!isGranted) {
+      isGranted = await Permission.contacts.request().isGranted;
+    }
+    if (isGranted) {
+      return await FastContacts.getAllContacts();
+    }
+    return [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Contact List"),
+          backgroundColor: Colors.orangeAccent,
+        ),
+        body: Container(
+          height: double.infinity,
+          child: FutureBuilder(
+            future: getContacts(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return const Center(
+                  child:
+                  SizedBox(height: 50, child: CircularProgressIndicator()),
+                );
+              }
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    Contact contact = snapshot.data[index];
+                    return Column(children: [
+                      ListTile(
+                        leading: const CircleAvatar(
+                          radius: 20,
+                          child: Icon(Icons.person),
+                        ),
+                        title: Text(contact.displayName),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(contact.phones[0].toString()),
+                            Text(contact.emails[0].toString()),
+                          ],
+                        ),
+                        onTap: () {},
+                      ),
+                      const Divider()
+                    ]);
+                  });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+}
