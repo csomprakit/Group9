53c316b688ae0596e135f54dd25ba86631ed1968
benz <csomprakit@seattleu.edu>
2024-03-10 19:55:55 -0700

    Updated bottomnav_test.dart

--- test/bottomnav_test.dart
+++ test/bottomnav_test.dart
@@ -1,89 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'dart:async';
-import 'package:recipe_book_app/bottom_nav.dart';
 import 'package:recipe_book_app/router.dart';
 import 'package:recipe_book_app/database/recipe_dao.dart';
 import 'package:recipe_book_app/database/recipe_entity.dart';
 
-
 class MockRecipeDao implements RecipeDao {
   List<RecipeEntity> _recipes = [];
   @override
-  Future<void> addRecipe(RecipeEntity event) async {}
+  Future<void> addRecipe(RecipeEntity recipe) async {
+    _recipes.add(recipe);
+  }
 
   @override
-  Stream<RecipeEntity?> findRecipeById(int id) {
-    // TODO: implement findRecipeById
+  Future<void> updateRecipe(RecipeEntity updatedRecipe) async {
+    final index = _recipes.indexWhere((recipe) => recipe.id == updatedRecipe.id);
+    if (index != -1) {
+      _recipes[index] = updatedRecipe;
+    }
+  }
+
+  @override
+  Future<void> deleteRecipe(RecipeEntity recipe) async {
+    _recipes.removeWhere((existingRecipe) => existingRecipe.id == recipe.id);
+  }
+
+  @override
+  Future<List<RecipeEntity>> listAllRecipes() {
+    return Future<List<RecipeEntity>>.value(_recipes);
+  }
+
+  @override
+  Future<int?> getRecipeRating(int id) {
+    // TODO: implement getRecipeRating
     throw UnimplementedError();
   }
 
   @override
-  Future<List<RecipeEntity>> listAllEvents() {
-    return Future.value(_recipes);
-  }
-
-  @override
-  Future<List<String>> listCategories() {
-    // TODO: implement listCategories
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> deleteRecipe(RecipeEntity event) {
-    // TODO: implement deleteRecipe
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> updateRecipe(RecipeEntity event) {
-    // TODO: implement updateRecipe
+  Future<void> updateRecipeRating(int id, int rating) {
+    // TODO: implement updateRecipeRating
     throw UnimplementedError();
   }
 }
 
 void main() {
-  var mockDao = MockRecipeDao();
+
   testWidgets('BottomNav "Settings" button test', (WidgetTester tester) async {
-    final router = await AppRouter(dao: mockDao).getRouter();
+    var mockDao = MockRecipeDao();
+    final router = AppRouter(dao: mockDao).getRouter();
+
     await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: BottomNav(),
-        ),
+      MaterialApp.router(
+        routerConfig: router,
       ),
     );
     await tester.tap(find.text('Settings'));
     await tester.pumpAndSettle();
-    expect(router.configuration.navigatorKey, '/settings');
-  });
-
-  testWidgets('BottomNav "Search" button test', (WidgetTester tester) async {
-    final router = await AppRouter(dao: mockDao).getRouter();
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: BottomNav(),
-        ),
-      ),
-    );
-
-    await tester.tap(find.text('Search'));
-    await tester.pumpAndSettle();
-    expect(router.configuration.navigatorKey, '/search');
   });
 
   testWidgets('BottomNav "Home" button test', (WidgetTester tester) async {
-    final router = await AppRouter(dao: mockDao).getRouter();
+    var mockDao = MockRecipeDao();
+    final router = AppRouter(dao: mockDao).getRouter();
+
     await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: BottomNav(),
-        ),
+      MaterialApp.router(
+        routerConfig: router,
       ),
     );
     await tester.tap(find.text('Home'));
     await tester.pumpAndSettle();
-    expect(router.configuration.navigatorKey, '/home');
   });
+
 }
